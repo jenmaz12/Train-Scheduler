0cab55ee5aa082fd4a68dcfe23c563e9c27c7fd6
jenmaz12 <jenmaz12@gmail.com>
2018-12-08 17:46:35 -0500

    set on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,32 @@ var config = {
     messagingSenderId: "187360712917"
   };
   firebase.initializeApp(config);
+// set database equal to a variable
+  var database = firebase.database();
+// set initial variables
+var train_name = "";
+var destination = "";
+var first_train = "";
+var frequency = "";
+
+// set on click event
+$("button").on("click", function(){
+    event.preventDefault();
+
+    train_name = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    first_train = $("#first-train").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    console.log(train_name);
+    console.log(destination);
+    console.log(first_train);
+    console.log(frequency);
+
+    database.ref().push({
+        train_name: train_name,
+        destination: destination,
+        first_train: first_train,
+        frequency: frequency
+    });
+});
