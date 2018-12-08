74215da340d68e5533e1c405078ce2085a16e35e
jenmaz12 <jenmaz12@gmail.com>
2018-12-07 22:02:54 -0500

    linking

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCoFcMGhawe0VwlO2d_rg2rVY79O_6XM9M",
+    authDomain: "train-scheduler-779b3.firebaseapp.com",
+    databaseURL: "https://train-scheduler-779b3.firebaseio.com",
+    projectId: "train-scheduler-779b3",
+    storageBucket: "",
+    messagingSenderId: "187360712917"
+  };
+  firebase.initializeApp(config);
